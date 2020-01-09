1fe1ce2d6eb7ced5ff0d3a05c523115ebbaac35b
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-09 01:50:56 -0700

    Add javascript script to change button text to Braille on mouseover

---
+++ app/assets/javascripts/button_braille.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  const button = document.getElementById('transform-button');
+  button.onmouseover = changeTextToBraille;
+  button.onmouseout = changeTextToEnglish;
+
+  function changeTextToBraille() {
+    event.target.value = '⠞⠗⠁⠝⠎⠋⠕⠗⠍';
+    event.target.className = 'braille_button';
+  };
+
+  function changeTextToEnglish() {
+    event.target.value = 'Transform';
+    event.target.className = '';
+  };
+};
