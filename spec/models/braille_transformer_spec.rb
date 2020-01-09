9817f94dddee938e7b9e6a01a2f07bff5f318d16
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-08 23:44:12 -0700

    Add unit test for grade_1_braille method on BrailleTransformer PORO

--- spec/models/braille_transformer_spec.rb
+++ spec/models/braille_transformer_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe BrailleTransformer, type: :model do
   describe 'instance methods' do
-    it 'can take an english string and return same string in grade 1 braille' do
-      string = "Hello, World!"
-      transformer = BrailleTransformer.new(string)
-      expect(transformer.to_braille).to eq('Hello, World!')
+    describe 'to_braille' do
+      it 'can take an english string and return same string in grade 1 braille' do
+        string = "Hello, World!"
+        transformer = BrailleTransformer.new(string)
+        expected = "⠠⠓⠑⠇⠇⠕⠠⠀⠠⠺⠕⠗⠇⠙⠮"
+        expect(transformer.grade_1_braille).to eq(expected)
+      end
     end
   end
 end
