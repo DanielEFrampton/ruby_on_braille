ef554c83e9203c7bd2657bb25e60b429b883fb75
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-08 23:45:23 -0700

    Complete feature test expectations for home index functionality

--- spec/features/home/index_spec.rb
+++ spec/features/home/index_spec.rb
@@ -42,13 +42,13 @@ RSpec.describe 'As a visitor', type: :feature do
 
     describe 'and I enter text into the text area and click submit' do
       before(:each) do
-        fill_ in 'text', with: 'Hello, World!'
+        fill_in 'text', with: 'Hello, World!'
 
         click_on('Transform')
       end
 
       it 'I should see the Braille corresponding to my text' do
-        unicode_string = ''
+        unicode_string = '⠠⠓⠑⠇⠇⠕⠠⠀⠠⠺⠕⠗⠇⠙⠮'
         expect(page).to have_content(unicode_string)
       end
     end
